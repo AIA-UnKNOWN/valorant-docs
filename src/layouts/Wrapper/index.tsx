cc6b44002afbe52840d65a913e9b9350c01c87ee
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-05-22 23:20:27 +0800

    added horizontal padding in mobile version of wrapper

--- src/layouts/Wrapper/index.tsx
+++ src/layouts/Wrapper/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Wrapper: React.FC = ({ children }) => {
   return (
-    <div className="max-w-[1200px] mx-auto">
+    <div className="px-3 lg:px-0 max-w-[1200px] mx-auto">
       {children}
     </div>
   );
